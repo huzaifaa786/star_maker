5ea3811508bde9e12fb990bd2ff6defb2eb21a19
usama <sohiusam1@gmail.com>
2024-02-23 16:52:52 +0500

    usama

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp(debugShowCheckedModeBanner: false,
       navigatorKey: navigatorKey,
       title: 'Star Maker',
       theme: ThemeData(
@@ -30,7 +30,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const OnBoardingView(),
+      home: const SplashView(),
     );
   }
 }
