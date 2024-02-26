50adb7e82a0aad9b47459f214e37ea8b690e41ff
Abdul Rehman <tritechuzaifa@gmail.com>
2024-02-26 15:28:09 +0500

    abdul rehman jr

--- lib/views/onboarding/onboarding.dart
+++ lib/views/onboarding/onboarding.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
 import 'package:star_maker/utils/colors.dart';
 import 'package:star_maker/views/Karaoke/component/logintype_card.dart';
